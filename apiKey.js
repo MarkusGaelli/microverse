da5d9beb778ca81c32d435b948afc347e867d497
Markus Gaelli <markus.gaelli@gmail.com>
2023-01-25 19:12:15 +0100

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
  /* Copy this into a file named apiKey.js */
 const apiKey = '1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs';
-const appId = 'com.gmail.markus.gaelli.microverse';
+const appId = ''io.github.markusgaelli.microverse';
 export default  {apiKey, appId};
